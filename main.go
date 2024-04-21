25bcbe69bcd8acb604bfe45a8a1328ddfe4763cf
Jay Zybura <arubyz@outlook.com>
2024-04-21 14:09:57 -0500

    gofmt

--- main.go
+++ main.go
@@ -21,29 +21,32 @@ gocog]]] */
 Package main creates an executable that will generate text from inline sourcecode.
 
 Usage:
-  gocog [OPTIONS] [INFILE | @FILELIST] ...
 
-  Runs gocog over each infile. 
-  Strings prepended with @ are assumed to be files continaing newline delimited lists of gocog command lines.
-  Command line options are passed to each command line in the file list, but options on the file list line
-  will override command line options. You may have filelists specified inside filelist files.
+	gocog [OPTIONS] [INFILE | @FILELIST] ...
+
+	Runs gocog over each infile.
+	Strings prepended with @ are assumed to be files continaing newline delimited lists of gocog command lines.
+	Command line options are passed to each command line in the file list, but options on the file list line
+	will override command line options. You may have filelists specified inside filelist files.
 
 Help Options:
-  -h, --help         Show this help message
+
+	-h, --help         Show this help message
 
 Application Options:
-  -z, --eof          The end marker can be assumed at eof.
-  -v, --verbose      enables verbose output
-  -q, --quiet        turns off all output
-  -S, --serial       Write to the specified cog files serially
-  -c, --cmd          The command used to run the generator code (go)
-  -a, --args         Comma separated arguments to cmd, %s for the code file
-                     ([run, %s])
-  -e, --ext          Extension to append to the generator filename (.go)
-  -M, --startmark    String that starts gocog statements ([[[)
-  -E, --endmark      String that ends gocog statements (]]])
-  -x, --excise       Excise all the generated output without running the
-                     generators.
-  -V, --version      Display the version of gocog
+
+	-z, --eof          The end marker can be assumed at eof.
+	-v, --verbose      enables verbose output
+	-q, --quiet        turns off all output
+	-S, --serial       Write to the specified cog files serially
+	-c, --cmd          The command used to run the generator code (go)
+	-a, --args         Comma separated arguments to cmd, %s for the code file
+	                   ([run, %s])
+	-e, --ext          Extension to append to the generator filename (.go)
+	-M, --startmark    String that starts gocog statements ([[[)
+	-E, --endmark      String that ends gocog statements (]]])
+	-x, --excise       Excise all the generated output without running the
+	                   generators.
+	-V, --version      Display the version of gocog
 */
 package main
